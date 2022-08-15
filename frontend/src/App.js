ca996e2f1b2a893a13588809a0d38e40e50c0fde
Apiwat Tatsanakitti <89071582+toonpublic99@users.noreply.github.com>
2022-08-15 08:14:43 +0700

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+// Created by Apiwat Tatsanakitti 
+
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const baseURL = "http://localhost:8000/api/TestDB"
+
+function App() {
+
+  const [TestDB, setProducts] = useState(null)
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setProducts(response.data);
+    });
+  }, [])
+
+  if (!TestDB) return null;
+
+  return (
+    <center><div>
+      <p>--------------------------------------------------------------------</p>
+      {
+       TestDB.map(element => {
+          return (
+            
+            <div key={element.id}>
+              <p>{element.data1} / {element.data2} / {element.data3}</p>
+              <p>--------------------------------------------------------------------</p>
+            </div>
+          )
+        })
+      }
+    </div></center>
+  );
+}
+
+export default App
