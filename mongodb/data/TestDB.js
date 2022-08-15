0bc43b021c5aab2d2e5d6869a80fd51110c16f8b
Apiwat Tatsanakitti <89071582+toonpublic99@users.noreply.github.com>
2022-08-16 02:02:15 +0700

    Add files via upload

---
+++ mongodb/data/TestDB.js
@@ -0,0 +1,21 @@
+// Created by Apiwat Tatsanakitti 
+
+db.createCollection("TestDB");
+db.TestDB.insert({
+      "id": 1,
+      "data1": "Document Name 1",
+      "data2": "123456789",
+      "data3": "picture-1.png"
+});
+db.TestDB.insert({
+      "id": 2,
+      "data1": "Document Name 2",
+      "data2": "99999.12345678",
+      "data3": "picture-2.png"
+});
+db.TestDB.insert({
+      "id": 3,
+      "data1": "Document Name 3",
+      "data2": "11110000",
+      "data3": "picture-3.png"
+});
